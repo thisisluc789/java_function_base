7b0f9067c6508052f0ddba8a468b5e3672682f22
Osvaldo Lucunde <osvaldoluc7@gmail.com>
2026-07-29 23:28:30 +0100

    recebaaaa

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,45 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+public class Main
+{
+    public static void main(String[] args)
+    {
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter three numbers: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        int higher = max(a, b, c);
+
+        showResult(higher);
+
+        sc.close();
+    }
+
+    public static int max(int x, int y, int z)
+    {
+        int aux;
+        if (x > y && x > z)
+        {
+            aux = x;
+        } else if (y > z)
+        {
+            aux = y;
         }
+        else
+        {
+            aux = z;
+        }
+        return aux;
+    }
+
+    public static void showResult(int value)
+    {
+        System.out.println("Higher = " + value);
     }
 }
